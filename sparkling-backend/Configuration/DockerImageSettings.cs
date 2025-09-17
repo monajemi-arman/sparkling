96d3d97a8a49c5c34ea495831e34d6a29fdcc874
Arman Monajemi <watbut67@gmail.com>
2025-09-17 11:46:29 +0330

    add localhost to image names so no download

--- sparkling-backend/Configuration/DockerImageSettings.cs
+++ sparkling-backend/Configuration/DockerImageSettings.cs
@@ -2,8 +2,9 @@ namespace Sparkling.Backend.Configuration;
 
 public class DockerImageSettings
 {
-    public string Spark { get; set; } = "spark-cuda";
-    public string Jupyter { get; set; } = "jupyter-custom";
+    // These local images are built before running the program using the build script in sparkling/docker-images/
+    public string Spark { get; set; } = "localhost/spark-cuda";
+    public string Jupyter { get; set; } = "localhost/jupyter-custom";
 
     public string SparkImageName => Spark.Contains(':') ? Spark.Split(':')[0] : Spark;
     public string SparkImageTag => Spark.Contains(':') ? Spark.Split(':')[1] : "latest";
